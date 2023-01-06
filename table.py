4205f2a042c3ab7cbd0b5074e8b76e24f4e6b40f
Mahan Abedini <MahanAbediny@yahoo.com>
2023-01-06 18:11:01 +0330

    Create table.py

---
+++ table.py
@@ -0,0 +1,6 @@
+for i in range(1,11):
+    for k in range (1,11):
+        sum = i * k 
+        print (sum , end=(" "))
+    print()
+#this code gaves you the result of   full multiplication of 1 to 10 
